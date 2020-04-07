8ede1b0942def2f17f80667217ec10dc12dfee2c
Pankaj Jangid <pankaj.jangid>
2020-04-07 14:17:16 +0530

    Added mapping profiles

---
+++ LeaveManagement/Mappings/Mapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LeaveManagement.Data;
+using LeaveManagement.Models.ViewModels;
+
+namespace LeaveManagement.Mappings
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+        }
+    }
+}
