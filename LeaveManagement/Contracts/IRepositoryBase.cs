bde18497d85ca0458fcd9f4982fb742ce17d8283
Pankaj Jangid <pankaj.jangid>
2020-04-07 11:34:27 +0530

    Added Generic BaseRepositoryInterface and common methods

---
+++ LeaveManagement/Contracts/IRepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Contracts
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        public TEntity GetById(int id);
+        public IEnumerable<TEntity> GetAll();
+        public void Add(TEntity entity);
+        public void Update(int id, TEntity entity);
+        public void Remove(TEntity entity);
+    }
+}
