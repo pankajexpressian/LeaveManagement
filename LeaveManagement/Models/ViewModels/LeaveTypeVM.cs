cf9d013a8687223d5a26753950424f4d45376a3b
Pankaj Jangid <pankaj.jangid>
2020-04-07 14:13:02 +0530

    Added view models for the entities

---
+++ LeaveManagement/Models/ViewModels/LeaveTypeVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace LeaveManagement.Models.ViewModels
+{
+    public class DetailsLeaveTypeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeVM
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
